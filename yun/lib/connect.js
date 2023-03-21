47a43ede4a672ae7177934455d614e8fc0f33353
longyf <Longyunfei@194>
2023-03-21 09:08:52 +0800

    123

--- yun/lib/connect.js
+++ yun/lib/connect.js
@@ -10,6 +10,11 @@ const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
 const host = 'http://8.142.81.91:1883';
 // const host = 'ws://localhost:1884';
 // var productKey = "a39ecc02a15affad254ff1a1ae8a244b"
+// const data4soil = '0101060000420DAC010A01010202030304040505010A01010202030304040505010A01010202030304040505010A01010202030304040505010A0101020203030404050502020046';//编码后字节流
+// const productkey1='74139f8ab541edb7a12a13eaeebaeada'//soil did
+// const productkey2='?气象站did'
+// const data4weather='0101060000420DAC030D0101020203030404050506060704020101'
+
 var productKey = "74139f8ab541edb7a12a13eaeebaeada"
 
 const options = {
