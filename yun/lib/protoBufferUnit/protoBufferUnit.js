a7dc0562e8d138e2c000915feed20a6793dee909
Xuyt <2338531179@qq.com>
2023-03-24 16:04:22 +0800

    add data handling and add payload_len

--- yun/lib/protoBufferUnit/protoBufferUnit.js
+++ yun/lib/protoBufferUnit/protoBufferUnit.js
@@ -2,13 +2,27 @@ const config = require('../../config/config.js');
 const PBConfig = config.pb;
 const protobuf = require('protobufjs');
 const fs = require('fs');
-// const protoFilePath = '../config/pbconfig/';
+const protoFilePath = '../../config/pbconfig/';
 const path = require('path');
 
 const root = protobuf.Root.fromJSON(require("../../config/pbConfig/protobufConfig.json"));
 const payloaddecode = require('../utils/payloadDecode')
  
 exports.ProtoBuf = PBtoJSON;
+
+function Datahandle(deviceType,output)
+{
+    folder_path = protoFilePath + deviceType+'.json'
+    var jsonconfig = require(folder_path)
+    for(key in jsonconfig){
+        var obj = jsonconfig[key]
+        if(obj.hasOwnProperty('divisor'))
+        {
+          output[key] = output[key]/10;
+        }
+    }
+    return output
+}
 
 function PBtoJSON (payload, productKey) {
  
@@ -28,20 +42,14 @@ function PBtoJSON (payload, productKey) {
       objects: false,  
       oneofs: false,
     });
-    // folder_path='../../config/pbconfig/'+PBConfig[productKey]
-    // // console.log(folder_path)
-
-    // var jsonconfig=require(folder_path)
-    // console.log(jsonconfig)
-
-    // for(key in jsonconfig){
-    //     var obj=jsonconfig[key]
+    output = Datahandle(deviceType,output)
   }
 
   else{
    var output = payloaddecode.rawdata2JS(payload, productKey)
   }
   
+  output.payload_len = payload.length
   var obj = {
     state:{reported:output},
   };
