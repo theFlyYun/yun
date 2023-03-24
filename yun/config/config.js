4812fb8c3ada4b35a9cbd99f982e0a0598a39902
Xuyt <2338531179@qq.com>
2023-03-24 20:22:14 +0800

    add pm2

--- yun/config/config.js
+++ yun/config/config.js
@@ -1,8 +1,7 @@
 module.exports = {
 pb: {
   'dc6ed37dca2e99ca79213e45bd1a1662': 'payload.Message',
-  '56f9c24fe95b094ae74b108599371a63': 'data_compression.SensorMessage',
-//   'a39ecc02a15affad254ff1a1ae8a244b': 'data_compression.DeviceState',
+  '56f9c24fe95b094ae74b108599371a63': 'soil.SoilMessage',
 },
 productkey2object:{
   "74139f8ab541edb7a12a13eaeebaeada":'soil.json',
