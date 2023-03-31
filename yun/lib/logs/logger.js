6f4d6560a64d96c523e5ed5edf6a96591a1d8436
longyf <Longyunfei@194>
2023-03-31 14:52:33 +0800

    add logger_v1

---
+++ yun/lib/logs/logger.js
@@ -0,0 +1,26 @@
+/* logger singleton */
+var winston = require('winston');
+// var moment = require('moment');
+// var _ = require('lodash');
+
+var defaultOptions = {
+  level: 'silly',
+  colorize: false,
+  humanReadable: false,
+};
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './logs/combined.log',level:'info' }),
+  ],
+});
+
+module.exports = logger;
