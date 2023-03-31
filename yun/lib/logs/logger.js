e8a13af25ee29e8af4b62edfecc6401462b7ad51
longyf <Longyunfei@194>
2023-03-31 16:53:48 +0800

    logger路径

--- yun/lib/logs/logger.js
+++ yun/lib/logs/logger.js
@@ -1,5 +1,6 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
+const path = require('path');
 const date = new Date();
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
@@ -17,9 +18,9 @@ const logger = createLogger({
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new transports.File({ filename: `./yun/lib/logs/logs/error/${date.toDateString()}.log`, level: 'error' }),
-    new transports.File({ filename: `./yun/lib/logs/logs/info/${date.toDateString()}.log`,level:'info' }),
-    new transports.File({ filename: `./yun/lib/logs/logs/warning/${date.toDateString()}.log`,level:'warning' }),
+    new transports.File({ filename: path.join(__dirname,`./logs/error/${date.toDateString()}.log`), level: 'error' }),
+    new transports.File({ filename: path.join(__dirname,`./logs/info/${date.toDateString()}.log`),level:'info' }),
+    new transports.File({ filename: path.join(__dirname,`./logs/warning/${date.toDateString()}.log`),level:'warning' }),
   ],
 });
 
