c00d5b412da2b9efee2213741cd8b5abfc672f70
longyf <Longyunfei@194>
2023-03-31 14:57:30 +0800

    logger

--- yun/lib/logs/logger.js
+++ yun/lib/logs/logger.js
@@ -18,8 +18,8 @@ const logger = winston.createLogger({
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: './logs/combined.log',level:'info' }),
+    new winston.transports.File({ filename: './yun/lib/logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './yun/lib/logs/combined.log',level:'info' }),
   ],
 });
 
