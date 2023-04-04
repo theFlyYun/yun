128601713de2351424d1d559b7b7048e7af92993
longyf <Longyunfei@194>
2023-04-04 13:01:37 +0800

    logger更改

--- yun/lib/logs/logger.js
+++ yun/lib/logs/logger.js
@@ -1,27 +1,65 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 const path = require('path');
-const date = new Date();
+// const date = new Date();
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-});
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   return `${timestamp} [${label}] ${level}: ${message}`;
+// });
 
+// const logger = createLogger({
+//   format: combine(
+//     label({ label: 'right meow!' }),
+//     timestamp(),
+//     myFormat
+//   ),
+//   transports: [
+//     //
+//     // - Write all logs with importance level of `error` or less to `error.log`
+//     // - Write all logs with importance level of `info` or less to `combined.log`
+//     //
+//     new transports.File({ filename: path.join(__dirname,`./logs/error/${date.toDateString()}.log`), level: 'error' }),
+//     new transports.File({ filename: path.join(__dirname,`./logs/info/${date.toDateString()}.log`),level:'info' }),
+//     new transports.File({ filename: path.join(__dirname,`./logs/warning/${date.toDateString()}.log`),level:'warning' }),
+//   ],
+// });
+
+// module.exports = logger;
+
+
+require("winston-daily-rotate-file");
+
+const customFormat = format.combine(
+    format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+    format.align(),
+    format.printf((i) => `${i.level}: ${[i.timestamp]}: ${i.message}`)
+);
+const defaultOptions = {
+    format: customFormat,
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d",
+};
 const logger = createLogger({
-  format: combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat
-  ),
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new transports.File({ filename: path.join(__dirname,`./logs/error/${date.toDateString()}.log`), level: 'error' }),
-    new transports.File({ filename: path.join(__dirname,`./logs/info/${date.toDateString()}.log`),level:'info' }),
-    new transports.File({ filename: path.join(__dirname,`./logs/warning/${date.toDateString()}.log`),level:'warning' }),
-  ],
+    format: customFormat,
+    transports: [
+        new transports.DailyRotateFile({
+            filename: path.join(__dirname,`./logs/info/%DATE%.log`),
+            level: "info",
+            ...defaultOptions,
+        }),
+        new transports.DailyRotateFile({
+            filename: path.join(__dirname,`./logs/error/%DATE%.log`),
+            level: "error",
+            ...defaultOptions,
+        }),
+        new transports.DailyRotateFile({
+          filename: path.join(__dirname,`./logs/warning/%DATE%.log`),
+          level: "warning",
+          ...defaultOptions,
+      }),
+    ],
 });
 
 module.exports = logger;
